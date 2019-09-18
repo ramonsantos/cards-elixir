e876ad60f0e5d94553e3fa97485d6b3152cd0723
Ramon Santos <ramonsantos.pe@gmail.com>
2019-09-18 12:19:45 -0300

    Refactoring

--- lib/cards.ex
+++ lib/cards.ex
@@ -30,11 +30,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "That file doesn't exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "That file doesn't exist"
     end
   end
 end
