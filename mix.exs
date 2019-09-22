baca1b84e7e784ed473deb07066117b435352020
ramonsantos <ramonsantos.pe@gmail.com>
2019-09-22 18:35:54 -0300

    Add ExCoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Cards.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps()
     ]
   end
@@ -21,7 +22,8 @@ defmodule Cards.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.21.2"}
+      {:ex_doc, "~> 0.21.2"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
